535f72bfe8371eef2508695ba3290cc8c6f4c62e
Tilio77 <tiliobolso1@gmail.com>
2022-11-20 18:41:03 -0300

    Project Finished

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import HeroImg2 from '../components/HeroImg2';
 
 const About = () => {
 	return (
 		<div>
-			About
+			<Navbar />
+			<HeroImg2 heading="ABOUT." text="Iam a friendly Front-End Developer." />
+			<Footer />
 		</div>
 	)
 }
